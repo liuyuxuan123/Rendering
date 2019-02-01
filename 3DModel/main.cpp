65e34db86bb794dc8c2d29eb5ade25db561db257
liuyuxuan123 <1010134430@qq.com>
2019-02-01 18:15:01 -0500

    Initial Commit

---
+++ 3DModel/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  3DModel
+//
+//  Created by 刘宇轩 on 2019/2/1.
+//  Copyright © 2019 yuxuanliu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
